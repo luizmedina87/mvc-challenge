fe85882894f9952206bcfceddccb404b1240d128
Luiz Medina <luizmedina87@gmail.com>
2022-06-15 07:46:22 -0400

    fixed connection bug

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,11 @@
 const Sequelize = require("sequelize");
 
-// making .env variables available
 require("dotenv").config();
 
-// connecting to the database
+// create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_PW, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: "localhost",
       dialect: "mysql",
       port: 3306,
