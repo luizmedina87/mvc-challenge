c377a637b1f243614e1830b0750777aa86f8ee81
Luiz Medina <luizmedina87@gmail.com>
2022-06-15 18:06:26 -0400

    implemented user CRUD and login/logout routes.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -66,3 +66,91 @@ router.post("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// logging in users
+router.post("/", (req, res) => {
+  // expects {"username": "Don Juan", "password": "12345"}
+  User.findOne({ where: { username: req.body.username } })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(400).json({ message: "No user with that username" });
+        return;
+      }
+      // checking password
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      // wrong password
+      if (!validPassword) {
+        res.status(400).json({ message: "Wrong password" });
+        return;
+      }
+      // right password
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+        res.json({
+          user: dbUserData,
+          message: "You are now logged in.",
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// logging out users
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end;
+  }
+});
+
+// updating user data
+router.put("/:id", (req, res) => {
+  // expects {"username": "Don James", "password": "12345"}
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "no user found for this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// deleting a user
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found for this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
