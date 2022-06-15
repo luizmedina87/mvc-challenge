5ed608b9291de1f391f38a011b11d7b68e40ce5e
Luiz Medina <luizmedina87@gmail.com>
2022-06-15 07:40:36 -0400

    added associations between tables

---
+++ models/index.js
@@ -0,0 +1,36 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+// User-Post association
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+// Comment-User-Post associations
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "SET NULL",
+});
+
+module.exports = { User, Post, Comment };
